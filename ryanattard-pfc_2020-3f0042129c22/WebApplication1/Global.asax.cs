823864a694211933e20345dd749d4fbf55df245d
jurgen <jurgen.schembri.a100175@mcast.edu.mt>
2020-04-21 14:10:21 +0200

    Uploading the full PFTC assignment

---
+++ ryanattard-pfc_2020-3f0042129c22/WebApplication1/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebApplication1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS",
+                Server.MapPath("jurgen-cloud-project-5f077f2e1ba1.json")
+                );
+        }
+    }
+}
